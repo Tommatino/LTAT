821a9841cefe06204512c70d7dfadf48c6c488de
Tommatino <tomowczarski@gmail.com>
2023-09-14 13:09:15 +0200

    add % form

--- src/Components/AlcoholDayForm/AlcoholDayForm.jsx
+++ src/Components/AlcoholDayForm/AlcoholDayForm.jsx
@@ -1,61 +1,72 @@
 import { useState } from "react";
 import styles from "./alcoholdayform.module.scss";
+import useAlcoholData from "../../Hooks/useAlcoholData.js";
 
 function AlcoholDayForm() {
-  const [days, setDays] = useState([1, 2, 3]);
-  const [currentDay, setCurrentDay] = useState({
-    lp: "",
-    data: "",
-    volume: 0,
-    relation: "",
-    burning: "",
-    promil: 0,
-    influence: "",
+  const { setAlcoholDay, getAlcoholDay } = useAlcoholData();
+  const [alcoholFormValues, setAlcoholFormValues] = useState({
+    alcoholML: 0,
+    alcoholPercentage: 0,
   });
+  const [dataList, setDataList] = useState([]);
 
-  const [volumeML, setVolumeML] = useState(0);
-  const [volumeGrams, setVolumeGrams] = useState(0);
-  const [alcoholPercentage, setAlcoholPercentage] = useState(0);
-  const [promileCalculated, setPromileCalculated] = useState(0);
-
-  const calculateGrams = () => {
-    const alcoholPure = volumeML * (alcoholPercentage / 100);
-    const alcoholGrams = (10 * alcoholPure) / 12.5;
-    setVolumeGrams(alcoholGrams);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setAlcoholFormValues((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
   };
+
+  // const calculateGrams = () => {
+  //   const alcoholPure = volumeML * (alcoholPercentage / 100);
+  //   const alcoholGrams = (10 * alcoholPure) / 12.5;
+  //   setVolumeGrams(alcoholGrams);
+  // };
 
   // const calculatePromile = () => {
   //   const promile = volumeGrams / (userParam.weight * {userParam.gender === "F" ? 0.6 : 0.7} );
   //   setPromileCalculated(promile)
   // };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setDataList((prev) => [...prev, alcoholFormValues]);
+    console.log(dataList);
+  };
+
   return (
     <section className={styles.form_container}>
-      <form className={styles.form}>
-        <button>
-          <span className="material-symbols-outlined">add_box</span>Dodaj dzień
-        </button>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <label>
           Ilość spoż. alkoholu [ml]:{" "}
           <input
             type="number"
-            value={volumeML}
-            onChange={(e) => setVolumeML(e.target.value)}
+            name="alcoholML"
+            value={alcoholFormValues.alcoholML}
+            onChange={handleChange}
           />
         </label>
         <label>
           Zawartość alkoholu [%]:{" "}
           <input
             type="number"
-            value={alcoholPercentage}
-            onChange={(e) => setAlcoholPercentage(e.target.value)}
+            name="alcoholPercentage"
+            value={alcoholFormValues.alcoholPercentage}
+            onChange={handleChange}
           />
         </label>
-        <button>
-          Dodaj pozycję i zsumuj{" "}
+        <button type="submit">
+          Dodaj pozycję
           <span className="material-symbols-outlined">add_box</span>
         </button>
       </form>
+
+      <button>
+        <span className="material-symbols-outlined">add_box</span>Zapisz
+      </button>
     </section>
   );
 }
---
+++ src/Hooks/useAlcoholData.js
@@ -0,0 +1,28 @@
+import { collection, doc, getDoc, setDoc } from "firebase/firestore";
+import { db, app } from "../firebase.js";
+import { getAuth } from "firebase/auth";
+import { useState } from "react";
+
+function useAlcoholData() {
+  const currentDay = new Date().toISOString().split("T")[0];
+  //format rok, miesiąc, dzień
+  const auth = getAuth(app);
+
+  const getAlcoholDay = async () => {
+    const docRef = doc(db, "alcoholStatistics", auth.currentUser.uid);
+    const docSnap = await getDoc(docRef);
+    return docSnap.data()?.[currentDay] || 0;
+    //day jest obiektem, [] dajemy aby użyć zmiennej
+  };
+  const setAlcoholDay = async (alcohol, prevAlcoholState) => {
+    const alcoholStatisticsRef = collection(db, "alcoholStatistics");
+    await setDoc(doc(alcoholStatisticsRef, auth.currentUser.uid)),
+      {
+        [currentDay]: alcohol + prevAlcoholState,
+      };
+  };
+
+  return { setAlcoholDay, getAlcoholDay };
+}
+
+export default useAlcoholData;
