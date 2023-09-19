0285a82a94f000fe189d689ea9c207fd5c1cc461
Tommatino <tomowczarski@gmail.com>
2023-09-19 11:35:16 +0200

    add chart

--- src/Components/StatisticsChart/StatisticsChart.jsx
+++ src/Components/StatisticsChart/StatisticsChart.jsx
@@ -1,21 +1,56 @@
 import styles from "./statisticschart.module.scss";
 import useUserData from "../../Hooks/useUserLogin.js";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import useAlcoholData from "../../Hooks/useAlcoholData.js";
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 
-function StatisticsChart(props) {
-  const user = useUserData();
-  const navigate = useNavigate();
+function StatisticsChart() {
+  const { getAlcoholData } = useAlcoholData();
+  const [alcoChartData, setAlcoChartData] = useState({});
 
-  // useEffect(() => {
-  //   if (!user) {
-  //     navigate("/signup");
-  //   }
-  // }, [user, navigate]);
+  useEffect(() => {
+    const getAlcoDays = async () => {
+      try {
+        const data = await getAlcoholData();
+        // https://github.com/machadop1407/chartjs-react-tutorial/blob/main/src/App.js
+        setAlcoChartData({
+          labels: Object.entries(data).map(([date]) => date),
+          datasets: [
+            {
+              label: "Alcohol Consumed",
+              data: Object.entries(data).map(
+                ([, alcoholGrams]) => alcoholGrams,
+              ),
+              backgroundColor: [
+                "rgba(75,192,192,1)",
+                "#ecf0f1",
+                "#50AF95",
+                "#f3ba2f",
+                "#2a71d0",
+              ],
+              borderColor: "black",
+              borderWidth: 2,
+            },
+          ],
+        });
+        console.log(Object.entries(data).map(([date]) => date));
+        console.log(
+          Object.entries(data).map(([, alcoholGrams]) => alcoholGrams),
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getAlcoDays();
+  }, []);
 
   return (
     <section className={styles.statistics}>
-      <div className={`${styles.statistics_container} container`}></div>
+      <div className={`${styles.statistics_container} container`}>
+        <Line data={alcoChartData} />
+      </div>
     </section>
   );
 }
