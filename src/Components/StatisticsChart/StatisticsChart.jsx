d4fbd3a3dbb03a19dd0e8e9d43374b82d3ee889e
Tommatino <tomowczarski@gmail.com>
2023-09-19 12:02:21 +0200

    fic chart and change library

--- src/Components/StatisticsChart/StatisticsChart.jsx
+++ src/Components/StatisticsChart/StatisticsChart.jsx
@@ -3,42 +3,26 @@ import useUserData from "../../Hooks/useUserLogin.js";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import useAlcoholData from "../../Hooks/useAlcoholData.js";
-import { Line } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 function StatisticsChart() {
   const { getAlcoholData } = useAlcoholData();
-  const [alcoChartData, setAlcoChartData] = useState({});
+  const [alcoChartData, setAlcoChartData] = useState([]);
 
   useEffect(() => {
     const getAlcoDays = async () => {
       try {
         const data = await getAlcoholData();
-        // https://github.com/machadop1407/chartjs-react-tutorial/blob/main/src/App.js
-        setAlcoChartData({
-          labels: Object.entries(data).map(([date]) => date),
-          datasets: [
-            {
-              label: "Alcohol Consumed",
-              data: Object.entries(data).map(
-                ([, alcoholGrams]) => alcoholGrams,
-              ),
-              backgroundColor: [
-                "rgba(75,192,192,1)",
-                "#ecf0f1",
-                "#50AF95",
-                "#f3ba2f",
-                "#2a71d0",
-              ],
-              borderColor: "black",
-              borderWidth: 2,
-            },
-          ],
-        });
-        console.log(Object.entries(data).map(([date]) => date));
-        console.log(
-          Object.entries(data).map(([, alcoholGrams]) => alcoholGrams),
-        );
+
+        setAlcoChartData(mappedData(data));
       } catch (err) {
         console.log(err);
       }
@@ -49,10 +33,24 @@ function StatisticsChart() {
   return (
     <section className={styles.statistics}>
       <div className={`${styles.statistics_container} container`}>
-        <Line data={alcoChartData} />
+        <LineChart width={600} height={400} data={alcoChartData}>
+          <Line type="monotone" dataKey="alcoData" stroke="#8884d8" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <CartesianGrid stroke="#f5f5f5" />
+          <Tooltip />
+          <Legend />
+        </LineChart>
       </div>
     </section>
   );
 }
 
 export default StatisticsChart;
+
+const mappedData = (data) => {
+  return Object.entries(data).map(([date, alcoData]) => ({
+    date: date,
+    alcoData: alcoData,
+  }));
+};
