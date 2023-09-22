707e1484647275f7d67251794a2fc4e0378d40fe
Tommatino <tomowczarski@gmail.com>
2023-09-22 12:55:25 +0200

    final vers

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,20 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const apiKey = import.meta.env.VITE_API_KEY;
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_API_KEY,
-  apiKey: "AIzaSyB8Oju2yO0vdbOCplRhB4kzig2IBDl2Uh8",
+  // eslint-disable-next-line no-undef
+  apiKey: apiKey,
   authDomain: "longtatest.firebaseapp.com",
   projectId: "longtatest",
   storageBucket: "longtatest.appspot.com",
   messagingSenderId: "524911211523",
   appId: "1:524911211523:web:3e665aa85c024521588a36",
 };
+
+console.log(apiKey);
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
