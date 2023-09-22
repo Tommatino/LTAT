b201acf7931c1118df4a94ffea789476689e323c
Tommatino <tomowczarski@gmail.com>
2023-09-22 10:19:09 +0200

    fix promile calc

--- src/Components/Row/Row.jsx
+++ src/Components/Row/Row.jsx
@@ -1,9 +1,21 @@
 import useAlcoholData from "../../Hooks/useAlcoholData.js";
+import { useEffect, useState } from "react";
 
 function Row({ date, alcoholGrams, lp, gender, weight, deleteRow }) {
   const { delAlcoholDay } = useAlcoholData();
-  let promileSwitch;
-  let switchText;
+  const [promileSwitch, setPromileSwitch] = useState(0);
+  const [switchText, setSwitchText] = useState(null);
+
+  useEffect(() => {
+    const factor = gender === "F" ? 0.6 : 0.7;
+    const promile = (alcoholGrams / (weight * factor)).toFixed(2);
+    setPromileSwitch(promile * 1);
+  }, [alcoholGrams, gender, weight]);
+
+  useEffect(() => {
+    switchFunction();
+  }, []);
+
   const relation = () => {
     if (gender === "F") {
       return alcoholGrams > 20
@@ -15,35 +27,33 @@ function Row({ date, alcoholGrams, lp, gender, weight, deleteRow }) {
       : "Nie przekroczono dziennego limitu dla mężczyzny";
   };
 
-  const calculatePromile = () => {
-    const factor = gender === "F" ? 0.6 : 0.7;
-    const promile = ((alcoholGrams / weight) * factor).toFixed(2);
-    promileSwitch = promile * 1;
-    return promile;
+  const switchFunction = () => {
+    if (promileSwitch >= 0.3 && promileSwitch <= 0.5) {
+      setSwitchText(
+        "Nieznaczne zaburzenia równowagi oraz euforia, zaburzenia widzenia",
+      );
+    } else if (promileSwitch > 0.5 && promileSwitch <= 0.7) {
+      setSwitchText("Zaburzenia sprawności ruchowej, obniżenie samokontroli");
+    } else if (promileSwitch > 0.7 && promileSwitch <= 2) {
+      setSwitchText(
+        "Zaburzenia równowagi, sprawności i koordynacji ruchowej, obniżenie progu bólu, spadek sprawności intelektualnej",
+      );
+    } else if (promileSwitch > 2 && promileSwitch <= 3) {
+      setSwitchText(
+        "Zaburzenia mowy (mowa bełkotliwa), wyraźne spowolnienie i zaburzenia równowagi, wzmożona senność, znaczne obniżona zdolność do kontroli własnych zachowań",
+      );
+    } else if (promileSwitch > 3 && promileSwitch <= 4) {
+      setSwitchText(
+        "Spadek ciśnienia krwi, obniżenie ciepłoty ciała, osłabienie lub zanik odruchów fizjologicznych oraz głębokie zaburzenia świadomości, poważne zagrożenie zdrowia człowieka",
+      );
+    } else if (promileSwitch > 4) {
+      setSwitchText(
+        "Głęboka śpiączka, zaburzenia czynności ośrodka oddechowego i naczyniowo-ruchowego, możliwość porażenia tych ośrodków przez alkohol, stan zagrożenia życia",
+      );
+    } else {
+      setSwitchText("Bez efektu");
+    }
   };
-
-  calculatePromile();
-
-  if (promileSwitch >= 0.3 && promileSwitch <= 0.5) {
-    switchText =
-      "Nieznaczne zaburzenia równowagi oraz euforia, zaburzenia widzenia";
-  } else if (promileSwitch > 0.5 && promileSwitch <= 0.7) {
-    switchText = "Zaburzenia sprawności ruchowej, obniżenie samokontroli";
-  } else if (promileSwitch > 0.7 && promileSwitch <= 2) {
-    switchText =
-      "Zaburzenia równowagi, sprawności i koordynacji ruchowej, obniżenie progu bólu, spadek sprawności intelektualnej";
-  } else if (promileSwitch > 2 && promileSwitch <= 3) {
-    switchText =
-      "Zaburzenia mowy (mowa bełkotliwa), wyraźne spowolnienie i zaburzenia równowagi, wzmożona senność, znaczne obniżona zdolność do kontroli własnych zachowań";
-  } else if (promileSwitch > 3 && promileSwitch <= 4) {
-    switchText =
-      "Spadek ciśnienia krwi, obniżenie ciepłoty ciała, osłabienie lub zanik odruchów fizjologicznych oraz głębokie zaburzenia świadomości, poważne zagrożenie zdrowia człowieka";
-  } else if (promileSwitch > 4) {
-    switchText =
-      "Głęboka śpiączka, zaburzenia czynności ośrodka oddechowego i naczyniowo-ruchowego, możliwość porażenia tych ośrodków przez alkohol, stan zagrożenia życia";
-  } else {
-    switchText = "Bez efektu";
-  }
 
   const handleClick = async () => {
     await delAlcoholDay(date);
